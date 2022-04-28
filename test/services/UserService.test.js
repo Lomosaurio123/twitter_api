b129e348539adb42482d211cf457ef2bb074f740
Mauricio Gael Hernandez Lom <mhernandezl1605@alumno.ipn.mx>
2022-04-28 18:56:59 -0500

    Cumplimiendo el requerimiento 2 usando TDD, pasando las prubeas al realizar las implementaciones solicitadas en la clase UserService

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -8,4 +8,13 @@ describe("Tests for UserService", () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     })
+
+    test("2. Get all user data in a list", () => {
+        const user = UserService.create(1, "carlogilmar", "Carlo")
+        const userInfoList = UserService.getInfo(user)
+        expect(userInfoList[0]).toBe(1)
+        expect(userInfoList[1]).toBe("carlogilmar")
+        expect(userInfoList[2]).toBe("Carlo")
+        expect(userInfoList[3]).toBe("Sin bio")
+    })
 })
